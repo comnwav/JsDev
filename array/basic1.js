5c53ce18e9a73ff6d3148815548117e72c845900
SEO HYEONIL <101534943+comnwav@users.noreply.github.com>
2022-04-25 15:53:37 +0900

    js

--- array/basic1.js
+++ array/basic1.js
@@ -5,10 +5,24 @@
 
 const numbers = [23, 4, 77, 51, 63];
 
+// 배열요소의 누적합
 let result = numbers.reduce(function (accu, e, ind, ary) {
     console.log(accu, e, ind, ary); // 0, 23, 0, 배열요소
     accu = accu + e; // 0 + 23 => 23, 27, 104
     return accu;
 }, 0);
 
+// 배열요소의 * 2 => 새로운 배열
+result = numbers.reduce(function (accu, e) {
+    return accu.push(e * 2);
+}, []);
+
+// filter : 50보다 큰 숫자를 가지는 새로운 배열
+result = numbers.reduce(function (acuu, e) {
+// result = numbers.reduceRight(function (acuu, e) { // reduceRight 는 오른쪽 부터 조짐
+    if(e >= 50) {
+        accu.push(e);
+    };
+    return accu; // 반환되는 값은 다음 순번의 초기값으로 사용됨
+}, []);
 console.log(result);
